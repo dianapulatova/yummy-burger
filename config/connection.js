7c123ae58c45aa6d7db5956e546f34461c9096b5
Diana Pulatova <dianapulatovaa@gmail.com>
2020-08-10 21:22:15 -0400

    added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,40 @@
 
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Afruza2017",
+//   database: "burgers_db",
+// });
+
+
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+
+// module.exports = connection;
+
+// 
+
+// Set up MySQL connection.
+require("dotenv").config();
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Afruza2017",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection(process.env.DB_URL);
+}
 
-
+// Make connection.
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -18,5 +43,5 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-
+// Export connection for our ORM to use.
 module.exports = connection;
