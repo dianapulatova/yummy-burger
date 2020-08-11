d088e386844dc20d93577a698dcdaa94a09785e6
Diana Pulatova <dianapulatovaa@gmail.com>
2020-08-10 23:14:20 -0400

    added picture of yummy burger

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,3 @@
-
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "Afruza2017",
-//   database: "burgers_db",
-// });
-
-
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-
-// module.exports = connection;
-
-// 
-
-// Set up MySQL connection.
 require("dotenv").config();
 var mysql = require("mysql");
 var connection;
