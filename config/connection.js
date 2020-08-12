28e6a26f341519cc30d4d97271073efd4c5bc549
Diana Pulatova <dianapulatovaa@gmail.com>
2020-08-12 19:33:31 -0400

    testing

--- config/connection.js
+++ config/connection.js
@@ -7,14 +7,14 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   //personal database
-  connection = mysql.createConnection(process.env.DB_URL);
-  // connection = mysql.createConnection({
-  //   host: process.env.DB_HOST,
-  //     port:process.env.DB_PORT,
-  //     user:process.env.DB_USER,
-  //     password:process.env.DB_PASSWORD,
-  //     database:process.env.DB_DATABASE,
-  // });
+  // connection = mysql.createConnection(process.env.DB_URL);
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+      port:process.env.DB_PORT,
+      user:process.env.DB_USER,
+      password:process.env.DB_PASSWORD,
+      database:process.env.DB_DATABASE,
+  });
 }
 
 // Make connection.
