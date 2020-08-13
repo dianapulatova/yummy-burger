e6263bccf663beebd4042c642ecdfc34c11514cd
Diana Pulatova <dianapulatovaa@gmail.com>
2020-08-12 21:27:49 -0400

    testing

--- config/connection.js
+++ config/connection.js
@@ -17,7 +17,6 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-// Make connection.
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -26,7 +25,6 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
 
 // // Set up MySQL connection.
